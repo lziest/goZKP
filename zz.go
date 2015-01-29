e88338d9ee75e5dbf01791206473ffc9095350ca
Zi Lin <zi@cloudflare.com>
2015-01-28 23:41:39 -0800

    add ZZ group

---
+++ zz.go
@@ -0,0 +1,34 @@
+package goZKP
+
+import (
+	"errors"
+	"math/big"
+)
+
+type ZZ struct {
+	g      *big.Int
+	modulo *big.Int
+}
+
+func (z *ZZ) Bytes() []byte {
+	return z.g.Bytes()
+}
+
+func (z *ZZ) Mul(x *ZZ) (*ZZ, error) {
+	if x.modulo.Cmp(z.modulo) != 0 {
+		return nil, errors.New("Unmatched modulo in ZZ multiplication.")
+	}
+	z.g.Mul(z.g, x.g)
+	z.g.Mod(z.g, z.modulo)
+	return z, nil
+}
+
+func (z *ZZ) Exp(x *big.Int) *ZZ {
+	z.g.Exp(z.g, x, z.modulo)
+	return x
+}
+
+func (z *ZZ) Inverse() *ZZ {
+	z.g.ModInverse(z.g, z.modulo)
+	return z
+}
