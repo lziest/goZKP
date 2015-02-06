316355d501460b7ba8cd43060d51d30239bb08b0
Zi Lin <zi@cloudflare.com>
2015-02-05 23:51:18 -0800

    Finish generalized Pedersen ZKP

--- pedersen/generalized.go
+++ pedersen/generalized.go
@@ -1,7 +1,6 @@
 package pedersen
 
 import (
-	"crypto/sha1"
 	"log"
 	"math/big"
 
@@ -25,4 +24,149 @@ type GeneralizedPedersenVerifier struct {
 	Bases []*big.Int // Group generators {g_i}
 	H     *big.Int   // Group generator h
 	Z     *big.Int   // public commitment,  z = (g**x) * (h**r)
+}
+
+func (p *GeneralizedPedersenProver) Commit() ([]*big.Int, error) {
+	rlist := []*big.Int{}
+	for _, x := range p.Exponents {
+		rx, err := x.Commit()
+		rlist = append(rlist, rx)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	rr, err := p.R.Commit()
+	if err != nil {
+		return nil, err
+	}
+
+	product := big.NewInt(1)
+	g := big.NewInt(0)
+	for i, rx := range rlist {
+		g.Exp(p.Bases[i], rx, p.P)
+		product.Mul(g, product)
+		product.Mod(product, p.P)
+		log.Print("g = ", p.Bases[i].Int64())
+		log.Print("g^rx = ", g.Int64())
+	}
+
+	h := big.NewInt(0)
+	h.Exp(p.H, rr, p.P)
+	log.Print("h = ", p.H.Int64())
+	log.Print("h^rr = ", h.Int64())
+
+	product.Mul(product, h)
+	product.Mod(product, p.P)
+
+	ret := []*big.Int{product}
+	return ret, nil
+}
+
+func (p *GeneralizedPedersenProver) Prove(c *big.Int) ([]*big.Int, error) {
+	if c.Cmp(p.Q) > 0 {
+		c.Mod(c, p.Q)
+	}
+
+	ret := []*big.Int{}
+	for _, x := range p.Exponents {
+		tx := x.Prove(c)
+		ret = append(ret, tx)
+	}
+	tr := p.R.Prove(c)
+	ret = append(ret, tr)
+
+	return ret, nil
+}
+
+func (p *GeneralizedPedersenProver) Sign(m *big.Int) ([]*big.Int, error) {
+	comm, err := p.Commit()
+	if err != nil {
+		return nil, err
+	}
+	comm = append([]*big.Int{m}, comm...)
+	c := Hash(p.Q, comm...)
+	proof, err := p.Prove(c)
+	if err != nil {
+		return nil, err
+	}
+
+	proof = append(proof, c)
+	return proof, nil
+}
+
+func (v *GeneralizedPedersenVerifier) ConsumeVerify(comm []*big.Int, c *big.Int, resp []*big.Int) (valid bool, remComm, remResp []*big.Int) {
+	valid = false
+	if len(resp) < len(v.Bases) {
+		return false, comm, resp
+	}
+
+	if len(comm) < 1 {
+		return false, comm, resp
+	}
+
+	var rv *big.Int
+	rv, remResp = v.RecoverCommitment(c, resp)
+
+	rc := comm[0]
+	remComm = comm[1:]
+
+	if rv.Cmp(rc) != 0 {
+		return
+	}
+	valid = true
+	return
+}
+
+func (v *GeneralizedPedersenVerifier) Verify(comm []*big.Int, c *big.Int, resp []*big.Int) bool {
+	valid, remComm, remResp := v.ConsumeVerify(comm, c, resp)
+
+	if len(remComm) != 0 || len(remResp) != 0 || !valid {
+		return false
+	}
+
+	return true
+}
+
+func (v *GeneralizedPedersenVerifier) RecoverCommitment(c *big.Int, resp []*big.Int) (rc *big.Int, remResp []*big.Int) {
+
+	rc = big.NewInt(1)
+
+	tmp := big.NewInt(0)
+	for i, base := range v.Bases {
+		tmp.Exp(base, resp[i], v.P)
+		rc.Mul(rc, tmp)
+		rc.Mod(rc, v.P)
+	}
+
+	sr := resp[len(v.Bases)]
+	tmp.Exp(v.H, sr, v.P)
+	rc.Mul(rc, tmp)
+	rc.Mod(rc, v.P)
+
+	remResp = resp[len(v.Bases)+1:]
+
+	return rc, remResp
+}
+
+func (v *GeneralizedPedersenVerifier) VerifySig(m *big.Int, resp []*big.Int) bool {
+	if len(resp) != 3 {
+		return false
+	}
+	remResp := resp[:len(resp)-1]
+	chlg := resp[len(resp)-1]
+
+	rv, remResp := v.RecoverCommitment(chlg, remResp)
+
+	if len(remResp) != 0 {
+		return false
+	}
+
+	// c = H(M, rv)
+	c := Hash(v.Q, m, rv)
+	if c.Cmp(chlg) != 0 {
+		return false
+	}
+	return true
+
 }
